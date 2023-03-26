9d1eaac72dc9dee9068a52df40c0316a198bc94e
KunalGoku <kunalbhargava182@gmail.com>
2023-03-27 00:19:54 +0530

    Initial commit

---
+++ app/src/main/java/com/gjr/navigationdrawersample/AFragment.java
@@ -0,0 +1,26 @@
+package com.gjr.navigationdrawersample;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class AFragment extends Fragment {
+
+
+public AFragment () {
+
+}
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_a, container, false);
+    }
+}
---
+++ app/src/main/java/com/gjr/navigationdrawersample/MainActivity.java
@@ -0,0 +1,93 @@
+package com.gjr.navigationdrawersample;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.google.android.material.navigation.NavigationView;
+
+public class MainActivity extends AppCompatActivity {
+DrawerLayout drawerLayout;
+NavigationView navigationView;
+Toolbar toolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        drawerLayout = findViewById(R.id.drawerlayout);
+        navigationView = findViewById(R.id.navvigationView);
+        toolbar = findViewById(R.id.toolbar);
+
+        //step 1
+
+        setSupportActionBar(toolbar);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawerLayout, toolbar, R.string.OpenDrawer, R.string.CloseDrawer);
+
+                drawerLayout.addDrawerListener(toggle);
+
+                toggle.syncState();
+
+                loadFragment(new AFragment());
+
+
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                int id = item.getItemId();
+
+                if (id == R.id.optNotes) {
+                    Toast.makeText(MainActivity.this, "notes", Toast.LENGTH_SHORT).show();
+
+
+
+                } else if (id == R.id.optHome) {
+                    loadFragment(new AFragment());
+                } else {
+                    Toast.makeText(MainActivity.this, "Settings", Toast.LENGTH_SHORT).show();
+                }
+
+                drawerLayout.closeDrawer(GravityCompat.START);
+
+                return true;
+            }
+        });
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+
+    private void loadFragment(Fragment fragemnt) {
+
+        FragmentManager fm = getSupportFragmentManager();
+        FragmentTransaction ft = fm.beginTransaction();
+
+        ft.add(R.id.container, fragemnt );
+        ft.commit();
+
+
+    }
+}
